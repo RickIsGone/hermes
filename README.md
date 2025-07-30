83576ea008d65c269cbe4a07bff00ef07894940a
riccardo <riccardotedeschi247@gmail.com>
2025-07-30 18:23:16 +0200

    new pcb and code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bike active aero handler
+this project is made for the **Hermes YSWS**  
+*Bike active aero handler* is a PCB which gets power from the bike's battery and based on the lean of the bike, which is obtained with the *BMI160* gyroscope controlls the active winglets moved by the 6 servomotors connected via the PWM pins 
+
+## The PCB
+![BOARD FRONT](board_front.png)
+![BOARD BACK](board_back.png)
--- src/hermes.cpp
+++ src/hermes.cpp
@@ -1,130 +1,86 @@
-/***************************************************************************
- * Example sketch for the MPU9250_WE library
- *
- * This sketch shows how to get the data from auto-calibration (offsets) and
- *reuse it. If you let line 20 commented, you will get the offsets. If you
- *uncomment line 20, you will apply the offsets.
- *
- * You can copy the offsets manually to reuse them, or you save them to / read
- *them from the EEPROM. For using the EEPROM uncomment line 21. If you use a non
- *AVR-based board you might need to adjust the code regarding the EEPROM use.
- *
- * For further information visit my blog:
- *
- * https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1
- *(German)
- * https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1
- *(English)
- *
- ***************************************************************************/
-
-/* Uncomment the following line after you have determined the offsets and copied
- * them */
-// #define REUSE_OFFSETS
-// #define USE_EEPROM
-#ifdef USE_EEPROM
-#include <EEPROM.h>
-#endif
-#include <MPU9250_WE.h>
 #include <Wire.h>
-constexpr unsigned MPU9250_ADDR = 0x68;
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <BMI160Gen.h>
+#include <cstdlib>
 
-MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
+// Display OLED (96x64)
+constexpr unsigned SCREEN_WIDTH = 96;
+constexpr unsigned SCREEN_HEIGHT = 64;
+constexpr unsigned SDA_GPIO = 21;
+constexpr unsigned SCL_GPIO = 22;
+constexpr unsigned BMI160_I2C_ADDRESS = 0x68;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 
 void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-  if (!myMPU9250.init()) {
-    Serial.println("MPU9250 does not respond");
-  } else {
-    Serial.println("MPU9250 is connected");
-  }
+   Serial.begin(115200);
+   delay(1000);
 
-  /* Apply the auto-calibration to determine the offsets for reuse.
-   * Uncomment "#define reuse_offsets" at the top if you want to reuse previous
-   * offsets.
-   */
-#ifndef REUSE_OFFSETS
-  Serial.println(
-      "Position you MPU9250 flat and don't move it - calibrating...");
-  delay(1000);
-  myMPU9250.autoOffsets();
-  Serial.println("Done!");
-  Serial.println();
+   // initialize I2C
+   Wire.begin(SDA_GPIO, SCL_GPIO);
 
-  /* Get the offsets from calibration and display them.
-   *  Uncomment line 20 if you want to reuse previous offsets.
-   */
-  xyzFloat aOffs = myMPU9250.getAccOffsets(); // get acceleration offsets
-  xyzFloat gOffs = myMPU9250.getGyrOffsets(); // get gyroscope offsets
+   // initialize display
+   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+      Serial.println("SSD1306 non found");
+      while (true)
+         ;
+   }
 
-  char buffer[35];
-  sprintf(buffer, "{%d.0, %d.0, %d.0}", (int)aOffs.x, (int)aOffs.y,
-          (int)aOffs.z);
-  Serial.print("Acceleration offsets: ");
-  Serial.println(buffer);
-  sprintf(buffer, "{%d.0, %d.0, %d.0}", (int)gOffs.x, (int)gOffs.y,
-          (int)gOffs.z);
-  Serial.print("Gyroscope offsets   : ");
-  Serial.println(buffer);
+   display.clearDisplay();
+   display.setTextSize(1);
+   display.setTextColor(SSD1306_WHITE);
+   display.setCursor(0, 0);
+   display.println("Init BMI160...");
+   display.display();
 
-#ifdef USE_EEPROM
-  EEPROM.put(0, aOffs);
-  EEPROM.put(sizeof(xyzFloat), gOffs);
-#endif // USE_EEPROM
-#endif // not REUSE_OFFSETS
-
-#ifdef REUSE_OFFSETS
-#ifndef USE_EEPROM
-  /* Now you can copy the offsets for reuse. Uncomment line 20. */
-  xyzFloat aOffs = {/* copy your offsets here for reuse */};
-  xyzFloat gOffs = {/* copy your offsets here for reuse */};
-#endif // not USE_EEPROM
-
-  /* Alternatively read your offsets back from EEPROM, if you have saved them
-   * there */
-#ifdef USE_EEPROM
-  xyzFloat aOffs = EEPROM.get(0, aOffs);
-  xyzFloat gOffs = EEPROM.get(sizeof(xyzFloat), gOffs);
-#endif // USE_EEPROM
-
-  myMPU9250.setAccOffsets(aOffs);
-  myMPU9250.setGyrOffsets(gOffs);
-#endif // REUSE_OFFSETS
-
-  /* Settings */
-  myMPU9250.enableGyrDLPF();
-  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
-  myMPU9250.setSampleRateDivider(5);
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
-  myMPU9250.enableAccDLPF(true);
-  myMPU9250.setAccDLPF(MPU9250_DLPF_6);
-  delay(200);
+   // initialize BMI160
+   if (!BMI160.begin(BMI160GenClass::I2C_MODE)) {
+      Serial.println("BMI160 not found, restarting firmware...");
+      display.display();
+      delay(2000);
+      exit(EXIT_FAILURE); // restart firmware
+   }
+   uint8_t dev_id = BMI160.getDeviceID();
+   display.print("DEVICE ID: ");
+   display.println(dev_id, HEX);
+   display.display();
+   BMI160.setGyroRange(250);
 }
 
 void loop() {
-/* _Now you can test your calibration data */
-#ifdef REUSE_OFFSETS
-  xyzFloat gValue = myMPU9250.getGValues();
-  xyzFloat gyr = myMPU9250.getGyrValues();
+   int gxRaw, gyRaw, gzRaw; // raw gyro values
+   float gx, gy, gz;
 
-  Serial.println("Acceleration in g (x,y,z):");
-  Serial.print(gValue.x);
-  Serial.print("   ");
-  Serial.print(gValue.y);
-  Serial.print("   ");
-  Serial.println(gValue.z);
+   // read raw gyro measurements from device
+   BMI160.readGyro(gxRaw, gyRaw, gzRaw);
 
-  Serial.println("Gyroscope data in degrees/s: ");
-  Serial.print(gyr.x);
-  Serial.print("   ");
-  Serial.print(gyr.y);
-  Serial.print("   ");
-  Serial.println(gyr.z);
+   // convert the raw gyro data to degrees/second
+   gx = convertRawGyro(gxRaw);
+   gy = convertRawGyro(gyRaw);
+   gz = convertRawGyro(gzRaw);
 
-  Serial.println("********************************************");
+   // display tab-separated gyro x/y/z values
+   display.clearDisplay();
+   display.setCursor(0, 10);
+   display.print("gx: ");
+   display.println(gx);
+   display.print("gy: ");
+   display.println(gy);
+   display.print("gz: ");
+   display.println(gz);
+   display.display();
 
-  delay(2000);
-#endif // REUSE_OFFSETS
+   delay(500);
+}
+
+float convertRawGyro(int gRaw) {
+   // since we are using 250 degrees/seconds range
+   // -250 maps to a raw value of -32768
+   // +250 maps to a raw value of 32767
+
+   float g = (gRaw * 250.0) / 32768.0;
+
+   return g;
 }
